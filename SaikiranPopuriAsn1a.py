84217b0a585d122434278442c3c0f1fcd431f1e0
Saikiran-Popuri <98856041+Saikiran-Popuri@users.noreply.github.com>
2022-02-15 17:51:55 -0500

    Add files via upload

---
+++ SaikiranPopuriAsn1a.py
@@ -0,0 +1,5 @@
+# Saikiran Popuri – Assignment 1A
+Distance = float(input('Enter Distance Value(miles):'))
+Speed = float(input('Enter Speed Value(milesPerhour):'))
+RouteTime = (Distance / Speed) * 60
+print('Route Time Value(minutes) =', round(RouteTime,1))
